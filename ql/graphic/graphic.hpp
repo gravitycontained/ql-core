3b09c4084c256a8ea0d8c4a61a50b839597dcc5e
DanielRabl <danielrabl1999@gmail.com>
2024-12-15 02:31:13 +0100

    interactive -> sync import in graphic root

--- ql/graphic/graphic.hpp
+++ ql/graphic/graphic.hpp
@@ -4,9 +4,9 @@
 
 #include <ql/graphic/state/state.hpp>
 #include <ql/graphic/event/event.hpp>
-#include <ql/graphic/interactive/interactive.hpp>
 #include <ql/graphic/render/render.hpp>
 #include <ql/graphic/resources/resources.hpp>
+#include <ql/graphic/sync/sync.hpp>
 
 #include <ql/graphic/util/util.hpp>
 
