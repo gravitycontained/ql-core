3ee92ce4b2ee455423d888ba75a0426ca6fe2340
DanielRabl <danielrabl1999@gmail.com>
2024-12-15 02:30:47 +0100

    removed old import from advanced-type root

--- ql/core/advanced-type/advanced-type.hpp
+++ ql/core/advanced-type/advanced-type.hpp
@@ -3,7 +3,6 @@
 #include <ql/core/advanced-type/camera/camera.hpp>
 #include <ql/core/advanced-type/color/color.hpp>
 #include <ql/core/advanced-type/container/container.hpp>
-#include <ql/core/advanced-type/interactive/interactive.hpp>
 #include <ql/core/advanced-type/iterative/iterative.hpp>
 #include <ql/core/advanced-type/fundamental/fundamental.hpp>
 #include <ql/core/advanced-type/signal/signal.hpp>
