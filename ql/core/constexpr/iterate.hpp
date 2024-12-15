d8ebd8d6237cfb95eb58ba0e06e24724ea602374
DanielRabl <danielrabl1999@gmail.com>
2024-12-15 02:28:19 +0100

    improved formatting & imports

--- ql/core/constexpr/iterate.hpp
+++ ql/core/constexpr/iterate.hpp
@@ -3,6 +3,8 @@
 #include <utility>
 #include <ql/core/definition/definition.hpp>
 #include <ql/core/constexpr/index.hpp>
+#include <ql/core/type/functional/functional.hpp>
+#include <ql/core/type/tuple/is-tuple.hpp>
 
 namespace ql
 {
@@ -10,7 +12,10 @@ namespace ql
 	constexpr void constexpr_iterate(F function)
 	{
 		auto unpack = [&]<ql::size... Ints>(std::index_sequence<Ints...>)
-		{ (std::forward<F>(function)(ql::constexpr_index<Ints, N>{}), ...); };
+		{
+			(std::forward<F>(function)(ql::constexpr_index<Ints, N>{}), ...);
+		};
+
 		unpack(std::make_index_sequence<N>());
 	}
 
