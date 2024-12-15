f15f9a7f7bcdb222b1ac080146e8341568960c72
DanielRabl <danielrabl1999@gmail.com>
2024-12-15 02:28:32 +0100

    updated injection to use new sync functions

--- ql/core/injection/injection.hpp
+++ ql/core/injection/injection.hpp
@@ -4,11 +4,7 @@
 
 #ifdef QL_GRAPHIC
 
-#include <ql/core/injection/provide.hpp>
-
-#include <ql/core/advanced-type/interactive/type.hpp>
-
-#include <ql/graphic/interactive/declare.hpp>
+#include <ql/graphic/sync/type.hpp>
 #include <ql/graphic/state/state-manager/state-manager.hpp>
 
 namespace ql
@@ -17,32 +13,80 @@ namespace ql
 	template <typename T>
 	struct injectable
 	{
-		ql_declare_interactive;
 		T* value = nullptr;
+		//std::shared_ptr<T> value = std::make_shared<T>();
+
+		~injectable()
+		{
+			ql::println(
+				ql::color::bright_yellow, "core ", ql::color::bright_gray, ":: ", ql::color::bright_blue,
+				ql::string_left_spaced("destruct injectable ", 24), ql::color::aqua, this, " ", ql::color::bright_blue, ql::type_name<T>()
+			);
+			this->value = nullptr;
+		}
 
 		void inject(T& value)
 		{
-			if (this->value == nullptr)
+			if (!this->value)
+			{
+				ql::println(
+					ql::color::bright_yellow, "core ", ql::color::bright_gray, ":: ", ql::color::bright_blue,
+					ql::string_left_spaced("received injection! ", 24), ql::color::aqua, this, " ", ql::color::bright_blue,
+					ql::type_name<T>()
+				);
+				//this->value = std::make_shared<T>(value);
 				this->value = &value;
+			}
 		}
 
 		void update_injection(ql::state_manager& manager)
 		{
-			if (this->value == nullptr)
+			if (!this->value)
 			{
-				ql::println(ql::color::bright_yellow, "core ", ql::color::bright_gray, ":: ", "update injection! ", ql::color::aqua, this);
+				ql::println(
+					ql::color::bright_yellow, "core ", ql::color::bright_gray, ":: ", ql::color::bright_blue,
+					ql::string_left_spaced("update injection ", 24), ql::color::aqua, this, " ", ql::color::bright_blue, ql::type_name<T>()
+				);
 				manager.last_state_provide();
 			}
 		}
 
-		constexpr operator T&()
+		constexpr operator bool()
 		{
+			return this->value != nullptr;
+		}
+
+		constexpr T*& operator->()
+		{
+			//return this->value.operator->();
+			return this->value;
+		}
+
+		
+		constexpr T& get()
+		{
+			if (!this->value)
+				throw std::runtime_error(ql::to_string("ql::injectable<", ql::type_name<T>(), ">: no injection received"));
+
+			return *this->value;
+		}
+
+		constexpr const T& get() const
+		{
+			if (!this->value)
+				throw std::runtime_error(ql::to_string("ql::injectable<", ql::type_name<T>(), ">: no injection received"));
+
 			return *this->value;
 		}
 
 		constexpr T& operator()()
 		{
-			return *this->value;
+			return this->get();
+		}
+
+		constexpr const T& operator()() const
+		{
+			return this->get();
 		}
 	};
 }
