cfc0cdec1b85ae90f0bc04d8980202f6bb5047c5
DanielRabl <danielrabl1999@gmail.com>
2024-12-15 02:25:12 +0100

    removed copies in effect function

--- ql/core/advanced-type/signal/effect.hpp
+++ ql/core/advanced-type/signal/effect.hpp
@@ -15,20 +15,16 @@ namespace ql
 	}
 
 	template <typename... Args>
+	requires (ql::are_signals<Args...>() && ql::is_callable<ql::variadic_type_back<Args...>>())
 	constexpr void effect(Args&&... objects)
-	requires (
-			ql::are_signals<Args...>() && ql::is_callable<ql::variadic_type_back<Args...>>()
-
-			/*
-			* use this when the active error is no longer hallucinated:
-
-			*/
-	)
 	{
-		auto&& function = ql::variadic_value_back(std::forward<Args>(objects)...);
+		auto&& tied = ql::auto_tie(std::forward<Args>(objects)...);
+		auto&& function = ql::tuple_value_back(tied);
 
 		constexpr auto size = ql::variadic_size<Args...>();
 		ql::constexpr_iterate<size - 1>([&](auto i)
-																		{ ql::tuple_value<i>(ql::auto_tie(std::forward<Args>(objects)...)).addListener(function); });
+		{
+			ql::tuple_value<i>(tied).addListener(function);
+		});
 	}
 }	 // namespace ql
