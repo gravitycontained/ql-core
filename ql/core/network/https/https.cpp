702ba385d47ee407bebe02c04974c223fef17290
DanielRabl <danielrabl1999@gmail.com>
2024-12-15 02:24:51 +0100

    added try - catch to http get

--- ql/core/network/https/https.cpp
+++ ql/core/network/https/https.cpp
@@ -3,7 +3,6 @@
 
 namespace ql
 {
-	// Helper function to handle the data from the GET request
 	size_t write_callback(void* contents, size_t size, size_t nmemb, void* user_data)
 	{
 		size_t total_size = size * nmemb;
@@ -11,33 +10,42 @@ namespace ql
 		return total_size;
 	}
 
-	// Function to perform an HTTP GET request using libcurl
 	ql::promise<std::string> http_get(const std::string& url)
 	{
 		return ql::make_promise(
 				[url]()
 				{
-					CURL* curl = curl_easy_init();
-					if (!curl)
+					try
 					{
-						throw std::runtime_error("Failed to initialize CURL");
-					}
+						CURL* curl = curl_easy_init();
+						if (!curl)
+						{
+							throw std::runtime_error("Failed to initialize CURL");
+						}
 
-					std::string response_data;
-					curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-					curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-					curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
-					curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);	 // Follow redirects
+						std::string response_data;
+						curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+						curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+						curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
+						curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);	 // Follow redirects
 
-					CURLcode res = curl_easy_perform(curl);
-					if (res != CURLE_OK)
-					{
+						CURLcode res = curl_easy_perform(curl);
+						if (res != CURLE_OK)
+						{
+							curl_easy_cleanup(curl);
+							throw std::runtime_error("CURL request failed: " + std::string(curl_easy_strerror(res)));
+						}
+
 						curl_easy_cleanup(curl);
-						throw std::runtime_error("CURL request failed: " + std::string(curl_easy_strerror(res)));
+						return response_data;
 					}
-
-					curl_easy_cleanup(curl);
-					return response_data;
+					catch (const std::exception& e)
+					{
+						ql::println(
+							ql::color::bright_yellow, "core", ql::color::bright_gray, " :: ", ql::color::bright_red, ql::to_string("http_get error: ", e.what())
+						);
+						return std::string();
+					}
 				}
 		);
 	}
