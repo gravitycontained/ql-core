d6536ff29c8f2f2d27ae087f850421e280c7fc81
DRL <d.rabl@friends-of-bytes.com>
2025-03-23 15:09:33 +0100

    added sync resolve and apply functions

--- ql/graphic/sync/type/type.hpp
+++ ql/graphic/sync/type/type.hpp
@@ -3,6 +3,7 @@
 #include <ql/core/definition/definition.hpp>
 #include <ql/core/serialize/serialize.hpp>
 #include <ql/core/advanced-type/container/bitset.hpp>
+#include <ql/core/transform/modal-apply.hpp>
 
 namespace ql
 {
@@ -57,7 +58,74 @@ namespace ql
 	template <typename C>
 	constexpr bool is_or_has_sync()
 	{
-		return is_sync<C>() || has_sync<C>();
+		return is_sync<C>() || has_sync<C>() || has_sync_extension<C>();
+	}
+
+
+	template <typename T, typename F>
+	constexpr void sync_modal_apply(T&& value, F function)
+	{
+		if constexpr (ql::is_sync<ql::modal_decay<decltype(value)>>())
+			ql::modal_apply(std::forward<decltype(value)>(value), function);
+
+		if constexpr (ql::has_sync<ql::modal_decay<decltype(value)>>())
+			ql::modal_apply(std::forward<decltype(value.sync)>(value.sync), function);
+
+		if constexpr (ql::has_sync_extension<ql::modal_decay<decltype(value)>>())
+			ql::modal_apply(std::forward<decltype(value.sync_extension)>(value.sync_extension), function);
+	}
+
+	template <bool order, typename T, typename F>
+	constexpr void sync_apply_soft(T&& value, F function)
+	{
+		if constexpr (order)
+		{
+			function(std::forward<decltype(value)>(value));
+
+			if constexpr (ql::has_sync<decltype(value)>())
+				function(std::forward<decltype(value.sync)>(value.sync));
+
+			if constexpr (ql::has_sync_extension<decltype(value)>())
+				function(std::forward<decltype(value.sync_extension)>(value.sync_extension));
+		}
+		else
+		{
+			if constexpr (ql::has_sync_extension<decltype(value)>())
+				function(std::forward<decltype(value.sync_extension)>(value.sync_extension));
+
+			if constexpr (ql::has_sync<decltype(value)>())
+				function(std::forward<decltype(value.sync)>(value.sync));
+
+			function(std::forward<decltype(value)>(value));
+		}
+	}
+
+	template <typename T>
+	requires (ql::is_or_has_sync<T>())
+	constexpr auto&& sync_resolve(T&& value)
+	{
+		if constexpr (ql::is_sync<decltype(value)>())
+			return std::forward<decltype(value)>(value);
+
+		else if constexpr (ql::has_sync<decltype(value)>())
+			return std::forward<decltype(value.sync)>(value.sync);
+
+		else
+			return std::forward<decltype(value.sync_extension)>(value.sync_extension);
+	}
+
+	template <typename T>
+	requires (ql::is_or_has_sync<T>())
+	constexpr auto&& sync_resolve_backwards(T&& value)
+	{
+		if constexpr (ql::has_sync_extension<decltype(value)>())
+			return std::forward<decltype(value.sync_extension)>(value.sync_extension);
+
+		else if constexpr (ql::has_sync<decltype(value)>())
+			return std::forward<decltype(value.sync)>(value.sync);
+
+		else
+			return std::forward<decltype(value)>(value);
 	}
 
 }	 // namespace ql
