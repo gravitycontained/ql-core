33133856df482936806ed73030ec420ebc737690
DRL <d.rabl@friends-of-bytes.com>
2025-03-24 11:31:55 +0100

    fixed curl url parameter being a std::string

--- ql/core/network/https/https.cpp
+++ ql/core/network/https/https.cpp
@@ -1,16 +1,19 @@
 #include <ql/core/network/https/https.hpp>
+#include <ql/core/system/system.hpp>
+
 #include <curl/curl.h>
+
 
 namespace ql
 {
-	size_t write_callback(void* contents, size_t size, size_t nmemb, void* user_data)
+	ql::size write_callback(void* contents, ql::size size, ql::size nmemb, void* user_data)
 	{
-		size_t total_size = size * nmemb;
+		ql::size total_size = size * nmemb;
 		static_cast<std::string*>(user_data)->append(static_cast<char*>(contents), total_size);
 		return total_size;
 	}
 
-	ql::promise<std::string> http_get(const std::string_view& url)
+	ql::promise<std::string> http_get(const std::string& url)
 	{
 		return ql::make_promise(
 				[url]()
@@ -19,12 +22,10 @@ namespace ql
 					{
 						CURL* curl = curl_easy_init();
 						if (!curl)
-						{
 							throw std::runtime_error("Failed to initialize CURL");
-						}
 
 						std::string response_data;
-						curl_easy_setopt(curl, CURLOPT_URL, url);
+						curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 						curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
 						curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
 						curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);	 // Follow redirects
--- ql/core/network/https/https.hpp
+++ ql/core/network/https/https.hpp
@@ -1,15 +1,13 @@
 #pragma once
 
 #include <ql/core/definition/definition.hpp>
-
-#include <ql/core/async/async.hpp>
-#include <curl/curl.h>
+#include <ql/core/async/promise.hpp>
 
 namespace ql
 {
 	// Helper function to handle the data from the GET request
-	QL_SOURCE size_t write_callback(void* contents, size_t size, size_t nmemb, void* user_data);
+	QL_SOURCE ql::size write_callback(void* contents, ql::size size, ql::size nmemb, void* user_data);
 
 	// Function to perform an HTTP GET request using libcurl
-	QL_SOURCE ql::promise<std::string> http_get(const std::string_view& url);
+	QL_SOURCE ql::promise<std::string> http_get(const std::string& url);
 }	 // namespace ql
