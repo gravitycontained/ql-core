6e2bb6a5e6c337a160ea084d5c036d22fdd3dffd
DRL <d.rabl@friends-of-bytes.com>
2025-03-30 22:14:40 +0200

    improved exponential moving average class by making it generic

--- ql/core/advanced-type/iterative/ema.hpp
+++ ql/core/advanced-type/iterative/ema.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <ql/core/definition/definition.hpp>
+#include <ql/core/transform/cast/cast.hpp>
+#include <ql/core/transform/limit.hpp>
 
 namespace ql
 {
+	template <typename T = ql::f64>
 	struct exponential_moving_average
 	{
 		exponential_moving_average(ql::f64 time_period = 5.0)
@@ -13,10 +16,26 @@ namespace ql
 
 		ql::f64 time_period;
 		ql::size time_ctr = 0u;
-		ql::f64 last_ma = 0.0;
+		T last_ma = 0.0;
 
-		QL_SOURCE void reset();
-		QL_SOURCE void add(ql::f64 value);
-		QL_SOURCE ql::f64 get_average() const;
+		void reset()
+		{
+			this->last_ma = T{};
+			this->time_ctr = 0;
+		}
+
+		void add(T value)
+		{
+			++this->time_ctr;
+			this->time_ctr = ql::min(ql::size_cast(this->time_period), this->time_ctr);
+
+			auto m = 2.0 / (1.0 + ql::f64_cast(this->time_ctr));
+			this->last_ma = m * value + (1 - m) * this->last_ma;
+		}
+
+		T get_average() const
+		{
+			return this->last_ma;
+		}
 	};
 }	 // namespace ql
