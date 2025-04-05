a39d2b37081b5064903d81dc1af563ddf179004a
DRL <d.rabl@friends-of-bytes.com>
2025-04-05 21:47:47 +0200

    removed generating warnings

--- ql/core/transform/slope/triangle.hpp
+++ ql/core/transform/slope/triangle.hpp
@@ -5,13 +5,13 @@ namespace ql
 	template <typename F>
 	constexpr F triangle_progression(F progress)
 	{
-		if (progress < 0.5)
+		if (progress < F{ 0.5 })
 		{
 			return progress * 2;
 		}
 		else
 		{
-			return (1 - (progress - 0.5) * 2);
+			return (1 - (progress - F{ 0.5 }) * 2);
 		}
 	}
 }
