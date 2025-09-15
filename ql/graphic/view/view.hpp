1ba468cc64c6906b3dfdd37565b654cacb0cdc7c
DRL <d.rabl@friends-of-bytes.com>
2025-09-15 20:49:01 +0200

    added view control type check

--- ql/graphic/view/view.hpp
+++ ql/graphic/view/view.hpp
@@ -23,12 +23,28 @@ namespace ql
 		struct view_signature<ql::view_type<T>> : std::true_type
 		{
 		};
+
+		template <typename T>
+		struct view_control_signature : std::false_type
+		{
+		};
+
+		template <typename T>
+		struct view_control_signature<ql::view_control_t<T>> : std::true_type
+		{
+		};
 	}	 // namespace detail
 
 	template <typename T>
 	constexpr bool is_view()
 	{
 		return detail::view_signature<ql::decay<T>>{};
+	}
+
+	template <typename T>
+	constexpr bool is_view_control()
+	{
+		return detail::view_control_signature<ql::decay<T>>{};
 	}
 
 	template <typename T>
